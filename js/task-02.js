0ff2ce0bd8b6aa6ee9c6b06de96f94c2f9e6a3c3
illya15 <113913094+illya15@users.noreply.github.com>
2023-02-17 15:18:11 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// const element = [];
+// for (let i = 0; i < ingredients.length; i++) {
+//   const option = ingredients[i];
+
+//   const liEl = document.createElement("li");
+//   liEl.textContent = option;
+//   liEl.classList.add("item");
+
+//   element.push(liEl);
+
+ 
+// }
+//  console.log(element);
+
+//  const ingredientsEl = document.querySelector(".ingredients");
+//  ingredientsEl.append(...element);
+
+ const element = ingredients.map(option => {
+
+
+  const liEl = document.createElement("li");
+  liEl.textContent = option;
+  liEl.classList.add("item");
+  return liEl;
+ })
+
+ console.log(element);
+ const ingredientsEl = document.querySelector(".ingredients");
+ingredientsEl.append(...element);
