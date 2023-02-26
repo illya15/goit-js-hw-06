e88785f934ec9a84385f9d2a1b5846647a3f5ba2
illya15 <113913094+illya15@users.noreply.github.com>
2023-02-26 20:45:28 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formEl = event.currentTarget.elements;
+    console.log(formEl);
+    const email = formEl.email.value;
+    console.log(email);
+    const password = formEl.password.value;
+    console.log(password);
+
+    const formData = {
+        email,
+        password,
+    }
+    if (email==="" || password==="") {
+        alert ("Будь-ласка, заповніть всі поля")
+     } else {
+        console.log(formData);
+     }
+     event.currentTarget.reset();
+
+    };
+
+
 
