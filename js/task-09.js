860b6a5cd64e6c8163488cbcf2ea9ad41b7b2343
illya15 <113913094+illya15@users.noreply.github.com>
2023-02-26 23:03:28 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(".widget");
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+  const result = getRandomHexColor();
+  bodyEl.style.backgroundColor = result;
+  spanEl.textContent = result;
+}
