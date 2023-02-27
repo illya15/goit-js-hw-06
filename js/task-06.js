16b64e693f0e39e508c30bb01998b76888417315
illya15 <113913094+illya15@users.noreply.github.com>
2023-02-27 22:00:38 +0200

    task6.1

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@
 const getText = document.querySelector("#validation-input");
 
 getText.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length === Number(getText.dataset)) {
+  if (event.currentTarget.value.length === Number(getText.dataset.length)) {
     getText.classList.remove("invalid");
     getText.classList.add("valid");
   } else {
